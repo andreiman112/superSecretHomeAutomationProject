2cece54826eb6812670e5bbae8ba330bd5300dc1
INTERN\andrei.man <andreiman112@gmail.com>
2017-04-11 10:49:33 +0300

    scos warninguri senzor lumina ambientala citire potentiometru incercat sezor PIR ADC-ul este partial facut

--- Tiva/Andrei/citire_pin2/adc_handler.c
+++ Tiva/Andrei/citire_pin2/adc_handler.c
@@ -20,8 +20,8 @@
 #define ADC0SS3_PRIO (0x02)  //Priority 2
 unsigned long Sensor_Temperature = 0;
 unsigned long Sensor_AnalogVoltage = 0;
-unsigned long adcChannels[12];
-unsigned long adcValues[2];
+uint32_t adcChannels[13];
+uint32_t adcValues[2];
  void Add_ADC_Channel(unsigned long channel)
 {
 	adcChannels[0]++;
@@ -30,27 +30,21 @@ unsigned long adcValues[2];
 void ADC_Init(void) //Initialize temperature sensor input
 {
 	unsigned char var=adcChannels[0]-1;
-	char i;
-	unsigned char step=0;
+	char i; 
 	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC1);  //The ADC1 peripheral must be enabled for use.
 
 	IntDisable(INT_ADC1SS2);
 	
-	ADCIntClear(ADC1_BASE, var);  //Clear interrupt flag	
+	ADCIntClear(ADC1_BASE, var); 
 	ADCIntDisable(ADC1_BASE, var);
-	ADCSequenceDisable(ADC1_BASE,var);  //Disable sequencer 3
+	ADCSequenceDisable(ADC1_BASE,var);  
 	
-  ADCSequenceConfigure(ADC1_BASE,var, /*ADC_TRIGGER_ALWAYS*/ADC_TRIGGER_PROCESSOR, 0); //Sequence 3 will do a single sample when the processor (SW) sends a signal to start the conversion
+  ADCSequenceConfigure(ADC1_BASE,var, /*ADC_TRIGGER_ALWAYS*/ADC_TRIGGER_PROCESSOR, 0); 
 	for(i=1;i<adcChannels[0];i++)
-	ADCSequenceStepConfigure(ADC1_BASE,var,i-1, adcChannels[i]);  //Configure step 0 on sequencer 3 to channel 1, interrupt enable and end conversion when after 1 conversion
+	ADCSequenceStepConfigure(ADC1_BASE,var,i-1, adcChannels[i]);  
  
-	ADCSequenceStepConfigure(ADC1_BASE,var,adcChannels[0]-1, adcChannels[adcChannels[0]]| ADC_CTL_IE | ADC_CTL_END);  //Configure step 0 on sequencer 3 to channel 1, interrupt enable and end conversion when after 1 conversion
-	 
-	// adcValues=(unsigned long*)malloc(sizeof(unsigned long)*adcChannels[0]);
-	//for(i=0;i<adcChannels[0];i++)
-//	adcValues[i]=0;
-	
-	ADCSequenceEnable(ADC1_BASE,var);  //Enable sequencer 3
+	ADCSequenceStepConfigure(ADC1_BASE,var,adcChannels[0]-1, adcChannels[adcChannels[0]]| ADC_CTL_IE | ADC_CTL_END);  
+	ADCSequenceEnable(ADC1_BASE,var);   
 	ADCIntEnable(ADC1_BASE, var); 
 		
 	IntPrioritySet(INT_ADC1SS2,(ADC1SS2_PRIO)<<5);
@@ -82,63 +76,11 @@ return adcValues[index];
 }
 void ADC1SS2_Handler(void) {  //ADC1 Seq3 ISR
 unsigned char var=2;
-	  
+	   
+ 
 	if(ADCIntStatus(ADC1_BASE, var, false))
 	{
 		ADCIntClear(ADC1_BASE, var);  //Clear interrupt flag
 		ADCSequenceDataGet(ADC1_BASE,var,  adcValues);
-		
-		
-		 //  Display_NewLine();
-			// Display_Decimal(Get_ADC_Value(0));
-		 //Display_Decimal(Get_ADC_Value(0));
-		
-		   //Display_NewLine();
-		 	// Display_Decimal(Get_ADC_Value(0));
-		 //Display_Decimal(adcValues[1]);
-		
-		 //  Display_NewLine();
-		 	// Display_Decimal(Get_ADC_Value(0));
-		/// Display_Decimal(adcValues[2]);
-	}		
-		/*
-		IntMasterDisable();	//Global interrupt disable
-		//Sensor_Temperature = (147.5 - ((75 * (ADC_Ref_Voltage) * digital_value) / 4095));
-		//Sensor_AnalogVoltage = ((digital_value * ADC_Ref_Voltage)/4096)*1000;
-		IntMasterEnable();	//Global interrupt enable
-		
-		Display_NewLine();
-		Display_String("PE2 ");			
-		Display_Decimal(digital_value[0]);
- 	
- 	  Display_NewLine();
-		Display_String("PD0 ");
-    Display_Decimal(digital_value[2]);
-		
-		
-		Display_NewLine();
-		Display_String("PD3 ");
-    Display_Decimal(digital_value[1]);
-		
-		Duty_Cycle(GPIO_PORTF_BASE,GPIO_PIN_2, digital_value[0]/4000.0*100.0);
-	 
-		
-//		Duty_Cycle(GPIO_PORTF_BASE,GPIO_PIN_2, duty_cycle);
-			
-			/*
- Display_Decimal(digital_value[2]);
-				Display_NewLine();
- Display_Decimal(digital_value[3]);*/
-		/*
-		The internal temperature sensor converts a temperature measurement into a voltage. This voltage
-		value, VTSENS, is given by the following equation (where TEMP is the temperature in °C):
-		VTSENS = 2.7 - ((TEMP + 55) / 75)
-		
-		The temperature sensor reading can be sampled in a sample sequence by setting the TSn bit in
-		the ADCSSCTLn register. The temperature reading from the temperature sensor can also be given
-		as a function of the ADC value. The following formula calculates temperature (TEMP in ?) based
-		on the ADC reading (ADCCODE, given as an unsigned decimal number from 0 to 4095) and the
-		maximum ADC voltage range (VREFP - VREFN):
-		TEMP = 147.5 - ((75 * (VREFP - VREFN) × ADCCODE) / 4096)
-		*/
+	}
 	}
