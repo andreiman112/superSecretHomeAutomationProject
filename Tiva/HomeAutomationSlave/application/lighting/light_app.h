8f16afad678c960ad856fc5ffb48120a78cacfa8
INTERN\alexandru.gaal <alexandru.gaal-ext@continental-corporation.com>
2017-05-10 15:22:36 +0300

    finished light dev

--- Tiva/HomeAutomationSlave/application/lighting/light_app.h
+++ Tiva/HomeAutomationSlave/application/lighting/light_app.h
@@ -3,26 +3,23 @@
 #include "stdbool.h"
 #include "stdint.h"
 
-/*-------------------Macro Definitions----------------*/
-#define NR_OF_LEDS (5)
-#define HighBit (0xF8)
-#define LowBit (0xE0)
-#define FullPower (0x80)
-#define BitMask (0x80)
-#define BargraphResolutionMultiplier (2)
-#define SET_RED		LedColor.Green = 0x00; LedColor.Red = FullPower; LedColor.Blue = 0x00; //Red Color
-#define SET_GREEN	LedColor.Green = FullPower; LedColor.Red = 0x00; LedColor.Blue = 0x00; //Green Color
-#define SET_BLUE	LedColor.Green = 0x00; LedColor.Red = 0x00; LedColor.Blue = FullPower; //Blue Color
-	
 typedef struct stRGB {
 	uint8_t Red;
 	uint8_t Green;
 	uint8_t Blue;
 } tstRGB;
 
-//void Send_RGB_Data(void);
+void LightHandler();
+
 void SetL1Red(uint8_t red);
 void SetL1Green(uint8_t green);
 void SetL1Blue(uint8_t blue);
 
+void SetL2Red(uint8_t red);
+void SetL2Green(uint8_t green);
+void SetL2Blue(uint8_t blue);
+
+void SetL3Red(uint8_t red);
+void SetL3Green(uint8_t green);
+void SetL3Blue(uint8_t blue);
 //EOF
