81434550d25f0ec9c19dd09804b24ed0db2de721
INTERN\alexandru.gaal <alexandru.gaal-ext@continental-corporation.com>
2017-05-10 10:22:05 +0300

    cleanup generated files

--- Tiva/HomeAutomationSlave/Objects/light_app.d
+++
@@ -1,6 +0,0 @@
-.\objects\light_app.o: application\lighting\light_app.c
-.\objects\light_app.o: application\lighting\light_app.h
-.\objects\light_app.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\objects\light_app.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\light_app.o: .\drivers\spi_handler\spi_handler.h
-.\objects\light_app.o: ..\..\..\TivaWare_C_Series-2.1.1.71\driverlib/sysctl.h
